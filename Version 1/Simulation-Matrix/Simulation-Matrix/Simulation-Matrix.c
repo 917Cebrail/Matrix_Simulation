e086c8694604540cf8108b1f71442a3cf0fce837
Cebrail Kutlar <cebolicebo917@gmail.com>
2024-06-19 20:13:47 +0300

    Add files via upload

---
+++ Version 1/Simulation-Matrix/Simulation-Matrix/Simulation-Matrix.c
@@ -0,0 +1,85 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#include <stdlib.h>
+#include "Source.h"
+
+#pragma warning(disable : 4996)
+
+int main()
+{
+	system("COLOR 0A");
+	// Main Declaretions
+	float _weight, _bias, _organism = 0;
+	printf("Enter organism count (O): ");
+	scanf("%f", &_organism);
+	float *outputs = (float *)malloc((_length * _organism) * sizeof(float));
+	float *_input = (float *)malloc((_length * _organism) * sizeof(float));
+	float *_outputs = (float *)malloc((_length * _organism) * sizeof(float));
+	char array[_array_length * 2];
+	int position = _array_length / 2;
+	int _position = 0;
+	srand((unsigned int)time(NULL));
+	// MATRIX AREA
+	for (int i = 0; i < _array_length * 2; i++) {
+		array[i] = '.';
+	}
+	array[position] = 'O';
+	printf("Enter weight (W): ");
+	scanf("%f", &_weight);
+	printf("Enter bias (B): ");
+	scanf("%f", &_bias);
+	// RANDOM INPUTS
+	for (int i = 0; i < _length * _organism; i++) {
+		_input[i] = (float)rand() / RAND_MAX * 2.0f - 1.0f;
+	}
+	// CREATE A ORGANISM
+	for (int count = 0; count < _organism; count++)
+	{
+		organism(_input + _length * count, _weight, _bias, _outputs + _length * count, outputs + _length * count);
+	}
+	// MEMORY CONTROL
+	if (_input == NULL || _outputs == NULL || outputs == NULL) {
+		printf(" Memory allocation failed\n");
+		_sleep(2000);
+		return 1;
+	}
+	else
+	{
+		// MATRIX MAIN CODE
+		printf("\n AI Trained. Simulation started...\n");
+		_sleep(2000);
+		system("cls");
+		for (int i = 0; i < _length * _organism; i++) {
+			for (int j = 0; j < _organism; j++) {
+				_move(array, _array_length * 2, &position, outputs[j * _length + i]);
+				for (int k = 0; k < _array_length * 2; k++) {
+					printf("%c", array[k]);
+				}
+				printf("\n");
+			}
+
+			for (int _counter = 0; _counter < _organism; _counter++) {
+				printf("%d. Organism Output: %f\n", _counter, outputs[_counter * _length + i]);
+			}
+
+			_sleep(500);
+			system("cls");
+		}
+		free(outputs);
+		free(_input);
+		free(_outputs);
+	}
+	return 0;
+}
+/*
+Agent Smith: Why, Mr. Anderson, why? Why, why, why!? Why are you doing this? Why? Why stand up? Why fight?
+Do you think you're fighting for what you believe in? For something beyond survival? 
+Can you tell me, do you know? Freedom? Truth? Maybe peace or love? Illusions, Mr. Anderson, delusions of perception... 
+temporary illusions produced by the feeble human intellect, 
+desperately trying to justify an existence devoid of any meaning or purpose, and all of them are as artificial as the Matrix. 
+After all, human intellect can invent such a poor concept as love. You can see that, Mr. Anderson, you need to understand that now! 
+You can't win! There's no point in fighting! Why, Mr. Anderson, why are you stubborn?
+
+Neo: Because it's my choice.
+*/
---
+++ Version 1/Simulation-Matrix/Simulation-Matrix/Source.h
@@ -0,0 +1,129 @@
+#ifndef SOURCE.H
+//#define SOURCE.H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+#define _length 40
+#define _array_length 40
+
+extern float sigmoid(float input, float weight, float bias)
+{
+	return 1 / (1 + exp(-((weight * input) + bias)));
+}
+
+extern float relu(float input)
+{
+	return max(0, input);
+}
+
+void softmax(float inputs[], int num_inputs, float outputs[]) {
+	float max_val = inputs[0];
+	float sum = 0.0;
+	for (int i = 1; i < num_inputs; ++i) {
+		if (inputs[i] > max_val) {
+			max_val = inputs[i];
+		}
+	}
+	for (int i = 0; i < num_inputs; ++i) {
+		outputs[i] = exp(inputs[i] - max_val);
+		sum += outputs[i];
+	}
+	for (int i = 0; i < num_inputs; ++i) {
+		outputs[i] /= sum;
+	}
+}
+
+bool hide_neuron(float _input, float _weight, float _bias)
+{
+	float w, b;
+	float result;
+	bool res;
+	w = 0; b = 0;
+	result = sigmoid(_input, _weight, _bias);
+	// printf(" [W] : %f | [B] : %f | [I] : %f\n", _weight, _bias, _input);
+	// printf(" [R] : %f\n", result);
+	if (fabs(result - 1.0) < 0.000001)
+	{
+		res = true;
+		// printf(" Result : %d\n", res);
+	}
+	else
+	{
+		for (int j = 0; j < _length; j++)
+		{
+			result = sigmoid(_input, _weight + j, _bias + j);
+			// printf(" %d. Attempt -> [R] : %f\n", j, result);
+			if (fabs(result - 1.0) < 0.000001)
+			{
+				res = true;
+				// printf(" Result : %d\n", res);
+				w = _weight + j;
+				b = _bias - j;
+				break;
+			}
+			else
+			{
+				res = false;
+				// printf(" Result : %d\n", res);
+			}
+		}
+	}
+	return res;
+}
+
+void organism(float _input[_length], float _weight, float _bias, float _outputs[_length], float outputs[_length])
+{
+	for (int i = 0; i < _length; i++) {
+		_input[i] = rand() % 5;
+	}
+	for (int k = 0; k < _length; k++) {
+		_outputs[k] = hide_neuron(_input[k], _weight, _bias);
+	}
+
+	float hidden_input[_length];
+	for (int i = 0; i < _length; i++) {
+		hidden_input[i] = (_outputs[i] + _input[i]) / 2.0;
+	}
+	softmax(hidden_input, _length, outputs);
+	// outputs : Move outputs
+}
+
+void _move(char array[], int length, int *position, float direction) {
+	int move_direction = 0;
+	if (direction <= 0.0490)
+	{
+		move_direction = 1;
+	}
+	else if (direction > 0.0500 && direction <= 0.0900) 
+	{
+		move_direction = -1;
+	}
+	else
+	{
+		if (direction < -1.0)
+		{
+			move_direction = -1;
+		}
+		else if (direction > 1.0)
+		{
+			move_direction = 1;
+		}
+		else
+		{
+			move_direction = direction;
+		}
+	}
+
+	int new_position = *position + move_direction;
+
+	if (new_position >= 0 && new_position < length) {
+		array[*position] = '.';
+		array[new_position] = 'O';
+		*position = new_position;
+	}
+}
+
+#endif // !Source.h
